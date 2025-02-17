9f4b70d2b5a5ab57a3eed59ab79a682ac7c8b620
BigMonk3 <ppears8473@gmail.com>
2025-02-17 12:39:12 -0700

    changed prompt to '$' because better, improved handling of empty ('\n') input

--- src/main.c
+++ src/main.c
@@ -4,9 +4,12 @@ int main()
 {
   while (1) { // Infinite while loop
 
-    fputs("| > ",stdout);
+    fputs("[]$ ",stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
+    
     char c;
+    if ((c = getchar()) == '\n') continue;
+    ungetc(c,stdin);
     while ((c = getchar()) != '\n') { 
       putchar(c);
     }
