8ee038709c7b35a7e42286a5c5d8051cf17a9765
BigMonk3 <ppears8473@gmail.com>
2025-02-24 12:32:53 -0700

    made ui into a command line style interface, added several commands incluind a help menu

--- src/ui.c
+++ src/ui.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h> // for strcmp()
 #include "tokenizer.h"
 #include "history.h"
 
@@ -7,6 +7,7 @@ int main()
 {
   char c;
   char str[100];
+  int f;
 
   printf("Type any string to be tokenized, -h for help\n");
 
@@ -18,15 +19,39 @@ int main()
     fgets(str, 100, stdin);
     char **tokens = tokenize(str);
 
-    switch(c) {
-      case ('!'):
+    if (!strcmp(*tokens, "-h"))
+    {
+      f = 1;
+    } else if (!strcmp(*tokens, "history"))
+    {
+      f = 2;
+    } else if (**tokens == '!')
+    {
+      f = 3;
+    } else if (!strcmp(*tokens, "-t"))
+    {
+      f = 4;
+    } else if (!strcmp(*tokens, "exit")) return 0;
+    else f = 0;
+
+    switch(f) {
+      case (1):
+        printf("help:\n\t-h: this dialogue\n\t");
+        printf("-t [string]: prints the tokenized [string]\n\t");
+        printf("history: shows a history of inputs\n\t");
+        printf("![index]: runs the input at [index] in history\n\t");
+        printf("exit: quits the program\n");
         break;
-      case ('-'):
+      case (2):
+        break;
+      case (3):
+        break;
+      case (4):
+        print_tokens(tokens+1);
         break;
       default:
-        print_tokens(tokens);
-        free_tokens(tokens);
+        printf("\tinvalid command; -h for help\n");
     }
-
+    free_tokens(tokens);
   }
 }
